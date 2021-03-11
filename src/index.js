914c65d7bab40b7432df045b0f67e77ef73a15a0
Olga <smirnova_olga18@mail.ru>
2021-03-11 12:36:44 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    let nStr = String(n);
+    if (nStr == '' || nStr == undefined) {
+        return 0;
+    } else {
+        let nReverse = nStr.split('').reverse().join('');
+        return Number(nReverse);
+    }
+};
+
+
+/* function reverse(n) {
+    n = Math.abs(n);
+    let nStr = String(n);
+    if (nStr == '' || nStr == undefined) {
+        return 0;
+    } else {
+        let nReverse = nStr.split('').reverse().join('');
+        return Number(nReverse);
+    }
 }
+
+console.log(reverse(170)); */
